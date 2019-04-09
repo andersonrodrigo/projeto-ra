5850cd1e11acb4750bab1e45919dd2492c53a8cc
Anderson Silva <anderson.silva@digicade.corp>
2019-04-09 10:11:00 -0300

    Responsible css

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -12,7 +12,7 @@ export default  props =>
 <div className="wrapper-content">
         <div className="box-tile">
             <img src="//upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/200px-React-icon.svg.png"/>
-             <h1 className="typing-element">Aplicação de Exemplo com ReactJS</h1>
+             <h1 className="typing-element">Aplicação com ReactJS</h1>
         </div>
         </div>
          
